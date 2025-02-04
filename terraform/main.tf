7aefdb66533170fb2677275902f4564c5a481363
Dave Furmidge <99021099+davefurmidge@users.noreply.github.com>
2025-02-04 21:29:32 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,29 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}-${var.environment}"
   location = var.location
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = var.virtual_network_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.virtual_network_name}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
+}
+
+# Create Public Subnet
+resource "azurerm_subnet" "public_subnet" {
+  name                 = "public-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Private Subnet
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
